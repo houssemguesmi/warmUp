396fd52d393c92751d8feb76ea15a7489b0ed2d6
houssemguesmi <houssemguesmi14@gmail.com>
2020-02-01 09:35:19 +0100

    WarmUp

--- warmUp1/warmUp1.js
+++ warmUp1/warmUp1.js
@@ -3,4 +3,29 @@
 // 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
 // 4-calculate your age in seconds.
 
+
 // your code is here
+function combine(firstName,lastName){
+    return firstName+' '+lastName;
+}
+//combine('Afif','Rakrouki') ==> 'Afif Rakrouki'
+function check(){
+    if ((13%3)===0){
+        return true
+    }
+    return false;
+}
+var arrx=[13,14,13,15,16,17,19,13,16,15]
+//check() ==> False
+function average(arrx){
+    s=0;
+    for(i=0;i<arrx.length){
+        s=s+arrx[i];
+    }
+    return s/arrx.length;
+}
+//average(arrx) ==> 15.1
+function ageInSec(age){
+return age*365*24*60*60;
+}
+//ageInSec(19) ==> 599184000
