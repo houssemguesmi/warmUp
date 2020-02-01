bf7f7bcbd3544df8301c32d4514eeced743e39b9
houssemguesmi <houssemguesmi14@gmail.com>
2020-02-01 13:24:35 +0100

    warmupfinished

--- warmUp1/warmUp1.js
+++ warmUp1/warmUp1.js
@@ -25,7 +25,25 @@ function average(arrx){
     return s/arrx.length;
 }
 //average(arrx) ==> 15.1
-function ageInSec(age){
-return age*365*24*60*60;
+function ageInSec(age,dayOfBirth,monthOfBirth,yearOfBirth){
+    today = new Date()
+    var year=today.getFullYear();
+    var month=today.getMonth();
+    var day=today.getDate();
+    var years=age;
+    if(month>monthOfBirth){
+    months=month-monthOfBirth-1;
+    }
+    else months=monthOfBirth-month-1;
+    if(day>dayOfBirth){
+        days=day-dayOfBirth;
+        }
+        else days=dayOfBirth-day;
+    console.log(years,months,days)
+    if(months%2 ===0){
+      var  monthsindays=((months/2)*30+(months/2)*31);
+    }else monthsindays=((months+1/2)*30+(months-1/2)*29)
+       var yearsindays=((years*12)/2*30+(years*12)/2*31)
+       return days+yearsindays+monthsindays;
 }
 //ageInSec(19) ==> 599184000
